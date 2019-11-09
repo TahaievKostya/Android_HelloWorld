8ecad880ee5ccb4253c34109bb7cfd9eef443bf0
TahaievKostya <kostiantyn.tahaiev@gmail.com>
2019-11-09 11:45:43 +0200

    Second commit

--- app/src/main/java/com/example/myfavoritemap/MainActivity.java
+++ app/src/main/java/com/example/myfavoritemap/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.myfavoritemap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button trueButton = findViewById(R.id.button_true);
+        Button falseButton = findViewById(R.id.button_false);
+
+        trueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, R.string.correct_toast, Toast.LENGTH_LONG).show();
+            }
+        });
+        falseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, R.string.incorrect_toast, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
